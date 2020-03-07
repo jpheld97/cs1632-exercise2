83e6aa90d3f01f126ea632c98c807d2e1aeec756
nleo575 <38141307+nleo575@users.noreply.github.com>
2020-03-07 15:34:22 -0500

    Modified generateID() to simplify computation.

--- src/Monkey.java
+++ src/Monkey.java
@@ -89,18 +89,20 @@ public class Monkey {
 	 */
 
 	public int generateId(int n) {
-		int toReturn = 100;
-		int bird = -900;
-		for (int l = 0; l < 1000; l++) {
-			for (int m = 0; m < 100; m++) {
-				bird += Math.round(Math.atan(l + m));
-				bird -= Math.round(Math.sin(m * m)) * Math.exp(m * m);
-				toReturn += bird;
-			}
-		}
-		toReturn += 10;
-		toReturn += n;
-		return toReturn;
+		// int toReturn = 100;
+		// int bird = -900;
+		// for (int l = 0; l < 1000; l++) {
+		// 	for (int m = 0; m < 100; m++) {
+		// 		bird += Math.round(Math.atan(l + m));
+		// 		bird -= Math.round(Math.sin(m * m)) * Math.exp(m * m);
+		// 		toReturn += bird;
+		// 	}
+		// }
+		// toReturn += 10;
+		// toReturn += n;
+		// toReturn always returns 223492 + n as the ID.
+
+		return 223492 + n;
 	}
 
 	/**
